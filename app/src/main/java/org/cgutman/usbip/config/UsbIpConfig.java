e8add35c606cf67326bd4946e332276b760a2f6b
gamesukimanIRS <26903162+gamesukimanIRS@users.noreply.github.com>
2024-02-07 23:32:47 +0900

    downgrade to API 31

--- app/src/main/java/org/cgutman/usbip/config/UsbIpConfig.java
+++ app/src/main/java/org/cgutman/usbip/config/UsbIpConfig.java
@@ -74,13 +74,13 @@ public class UsbIpConfig extends ComponentActivity {
 					stopService(new Intent(UsbIpConfig.this, UsbIpService.class));
 				}
 				else {
-//					if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-						if (ContextCompat.checkSelfPermission(UsbIpConfig.this, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
-							startService(new Intent(UsbIpConfig.this, UsbIpService.class));
+					startService(new Intent(UsbIpConfig.this, UsbIpService.class));
+					/*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+						if (ContextCompat.checkSelfPermission(UsbIpConfig.this, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED){
 						} else {
 							requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS);
 						}
-//					}
+					}*/
 				}
 				
 				running = !running;
